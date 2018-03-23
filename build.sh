c8256e2f1822f3f0020451c2e6726cbcd0e4750d
DhirajSurvase <dms25121997@gmail.com>
2018-03-23 06:12:18 +0000

    update build script
    clone gcc 7.x tc

--- build.sh
+++ build.sh
@@ -23,9 +23,12 @@ export KBUILD_BUILD_HOST="Dracarys"
 #export CROSS_COMPILE=/home/ubuntu/kcp/gcc4.8/bin/arm-eabi-
 
 #get toolchain gcc-4.9
-git clone https://github.com/DhirajSurvase/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 gcc4.9
-export CROSS_COMPILE=/home/younilban/kernel/panelli/gcc4.9/bin/arm-linux-androideabi-
+#git clone https://github.com/DhirajSurvase/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 gcc4.9
+#export CROSS_COMPILE=/home/younilban/kernel/panelli/gcc4.9/bin/arm-linux-androideabi-
 
+#get toolchain gcc-gnu-7.x
+git clone https://github.com/nathanchance/gcc-prebuilts -b arm-gnu-7.x arm-gnu-7.x
+export CROSS_COMPILE=/home/younilban/kernel/panelli/arm-gnu-7.x/bin/arm-gnu-linux-androideabi-
 
 #Defconfig for Moto C plus
 #make -C $PWD O=$PWD/out ARCH=arm A158_defconfig
